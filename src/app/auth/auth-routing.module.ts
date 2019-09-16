2d929619707436c36bc0be8b09f0e9e7e004f84a
Hassan Sani <saniyhassan@gmail.com>
2019-09-16 23:50:57 +0100

    implemented register component

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: '/auth' },
-    { path: '', component: AuthComponent, children: [
-        { path: 'login', component: LoginComponent, outlet: 'authspace' },
-    ] },
+    { path: '', component: AuthComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { AuthComponent } from './auth.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
-  declarations: [LoginComponent, AuthComponent],
+  declarations: [LoginComponent, AuthComponent, RegisterComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
