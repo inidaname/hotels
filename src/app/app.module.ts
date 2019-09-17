e8a30f0fc4dedd3eeadd85d79399940f758f1ac8
Hassan Sani <saniyhassan@gmail.com>
2019-09-17 03:01:02 +0100

    added fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NotFoundComponent } from './home/not-found/not-found.component';
@@ -21,7 +20,6 @@ import { NotFoundComponent } from './home/not-found/not-found.component';
     AppRoutingModule,
     BrowserModule,
     NgbModule,
-    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
