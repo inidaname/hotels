ac62e2c404b4909779e5f2e7c6d6c032e59985c1
Hassan Sani <saniyhassan@gmail.com>
2019-09-17 01:01:19 +0100

    implementing home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
-  },
+    path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)  },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
 ];
 
 @NgModule({
