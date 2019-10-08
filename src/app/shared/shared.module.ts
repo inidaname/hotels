f54c8432679f249107240f8fedc59890c45241ef
Hassan Sani <saniyhassan@gmail.com>
2019-10-09 00:47:09 +0100

    created header and sidebar components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HeaderInterceptor, JwtInterceptor } from './interceptor';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, SidebarComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    SidebarComponent
   ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
-      useClass: HeaderInterceptor
+      useClass: HeaderInterceptor,
+      multi: true
     },
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
   ]
