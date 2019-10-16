14da3e78d48a528e66ec6fb40d5e7fa94e13c0b6
Hassan Sani <saniyhassan@gmail.com>
2019-10-16 10:18:45 +0100

    created product component

--- src/app/frontdesk/frontdesk-routing.module.ts
+++ src/app/frontdesk/frontdesk-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PathNotFoundComponent } from '@shared/path-not-found/path-not-found.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'products' },
+  { path: 'products', component: ProductsComponent },
+  { path: '**', component: PathNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/frontdesk/frontdesk.module.ts
+++ src/app/frontdesk/frontdesk.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FrontdeskRoutingModule } from './frontdesk-routing.module';
+import { SharedModule } from '@shared/shared.module';
+import { ProductsComponent } from './products/products.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductsComponent],
   imports: [
     CommonModule,
-    FrontdeskRoutingModule
+    FrontdeskRoutingModule,
+    SharedModule
   ]
 })
 export class FrontdeskModule { }
