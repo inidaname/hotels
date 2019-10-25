19e4e16250cb66f57f83a4e936f4d6a984fc679e
Hassan Sani <saniyhassan@gmail.com>
2019-10-25 12:39:24 +0100

    working on implementing components

---
+++ src/app/hotel-services/guests/guests.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './guests.component.html',
+  styleUrls: ['./guests.component.scss']
+})
+export class GuestsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
