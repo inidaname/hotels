794a20b41422126e7451d0f070598cf023f8c339
Hassan Sani <saniyhassan@gmail.com>
2019-11-05 00:42:20 +0100

    product details

--- src/app/point-of-sales/point-of-sales.module.ts
+++ src/app/point-of-sales/point-of-sales.module.ts
@@ -3,10 +3,14 @@ import { SharedModule } from '@shared/shared.module';
 
 import { PointOfSalesRoutingModule } from './point-of-sales-routing.module';
 import { ProductListsComponent } from './product-lists/product-lists.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 @NgModule({
-  declarations: [ProductListsComponent],
+  declarations: [
+    ProductListsComponent,
+    ProductDetailsComponent
+  ],
   imports: [
     SharedModule,
     PointOfSalesRoutingModule
