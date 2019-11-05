3b64c79608c2519c4a56885127753b7704d8a069
Hassan Sani <saniyhassan@gmail.com>
2019-11-05 11:58:55 +0100

    implemented admin module

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { SharedModule } from '@shared/shared.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    AdminRoutingModule,
+    SharedModule
+  ]
+})
+export class AdminModule { }
