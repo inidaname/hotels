dd05f667fdc896694a6349668a4beca8d70b2632
Hassan Sani <saniyhassan@gmail.com>
2019-11-06 16:27:33 +0100

    working on product table

--- src/app/admin/stock/stock.component.ts
+++ src/app/admin/stock/stock.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { SortableDirective, SortEvent } from '@directives/sortable.directive';
 import { ProductFormComponent } from '@components/product-form/product-form.component';
+import { CountryService } from '@services/countries.service';
+import { Observable } from 'rxjs';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
+  providers: [CountryService, DecimalPipe]
 })
 export class StockComponent implements OnInit {
 
+  @ViewChildren(SortableDirective) headers: QueryList<SortableDirective>;
+
+  countries$: Observable<any[]>;
+  total$: Observable<number>;
 
   constructor(
-    private modal: NgbModal
-  ) {
-  }
+    private modal: NgbModal,
+    public service: CountryService
+  ) {}
 
   ngOnInit() {
   }
@@ -20,6 +30,18 @@ export class StockComponent implements OnInit {
   openModal() {
     const newModal = this.modal.open(ProductFormComponent);
     newModal.componentInstance.name = 'Get';
+  }
+
+  onSort({column, direction}: SortEvent) {
+    // resetting other headers
+    this.headers.forEach(header => {
+      if (header.sortable !== column) {
+        header.direction = '';
+      }
+    });
+
+    this.service.sortColumn = column;
+    this.service.sortDirection = direction;
   }
 
 }
