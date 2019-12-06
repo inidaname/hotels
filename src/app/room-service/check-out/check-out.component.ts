fef07f754c0d26666de160819dc8f913ef9d865a
Hassan Sani <saniyhassan@gmail.com>
2019-12-06 03:38:45 +0100

    worked on checkout for room

---
+++ src/app/room-service/check-out/check-out.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './check-out.component.html',
+  styleUrls: ['./check-out.component.scss']
+})
+export class CheckOutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
