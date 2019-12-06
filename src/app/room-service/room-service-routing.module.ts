eb4b52e96295652ca186046fa0b1a5562ddfc9a9
Hassan Sani <saniyhassan@gmail.com>
2019-12-06 03:38:25 +0100

    worked on other services

--- src/app/room-service/room-service-routing.module.ts
+++ src/app/room-service/room-service-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { RoomListComponent } from './room-list/room-list.component';
 import { OtherServiceComponent } from './other-service/other-service.component';
 import { PathNotFoundComponent } from '@components/path-not-found/path-not-found.component';
+import { CheckOutComponent } from './check-out/check-out.component';
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'roomslist' },
   { path: 'roomslist', component: RoomListComponent },
   { path: 'otherservice', component: OtherServiceComponent },
+  { path: 'checkout', component: CheckOutComponent },
   { path: '**', component: PathNotFoundComponent }
 ];
 
--- src/app/room-service/room-service.module.ts
+++ src/app/room-service/room-service.module.ts
@@ -4,12 +4,14 @@ import { RoomServiceRoutingModule } from './room-service-routing.module';
 import { SharedModule } from '@shared/shared.module';
 import { RoomListComponent } from './room-list/room-list.component';
 import { OtherServiceComponent } from './other-service/other-service.component';
+import { CheckOutComponent } from './check-out/check-out.component';
 
 
 @NgModule({
   declarations: [
     RoomListComponent,
-    OtherServiceComponent
+    OtherServiceComponent,
+    CheckOutComponent
   ],
   imports: [
     SharedModule,
