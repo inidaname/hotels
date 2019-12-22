90ac6a22f5fe4ff0fc505607053dd2cf403d9997
Hassan Sani <saniyhassan@gmail.com>
2019-12-22 11:35:12 +0100

    working on super admin

---
+++ src/app/home/rooms/rooms.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '@services/api.service';
+
+@Component({
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.scss']
+})
+export class RoomsComponent implements OnInit {
+
+  constructor(
+    private api: ApiService
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
