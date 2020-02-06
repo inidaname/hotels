a7d70bede3042bd40e4e8c9f6db3e88b1b1c9c18
Hassan Sani <saniyhassan@gmail.com>
2020-02-06 01:46:45 +0100

    added network check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserDataService } from './shared/services/user-data.service';
 import { ApiService } from './shared/services/api.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +11,28 @@ import { ApiService } from './shared/services/api.service';
 })
 export class AppComponent implements OnInit {
 
+  @ViewChild('content', {static: true}) content;
+
   constructor(
     private data: UserDataService,
-    private api: ApiService
+    private api: ApiService,
+    private spinner: NgxSpinnerService,
+    private modal: NgbModal
   ) {}
 
   ngOnInit(): void {
+    this.spinner.show();
     if (localStorage.getItem('currentUser')) {
       this.api.getUserById().subscribe(user => {
+        this.spinner.hide();
         return user;
+      },
+      err => {
+        this.spinner.hide();
+        this.modal.open(this.content);
       });
+    } else {
+      this.spinner.hide();
     }
   }
 }
